58b3b80902a336240cc522857e13c0534fca5727
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2025-02-15 15:11:06 -0500

    New start.

---
+++ README.md
@@ -0,0 +1,15 @@
+# CSCI4060U Final
+
+1. Initialize particles
+2. Sort into bins.
+   + Bin size based on interaction radius.
+3. Simulate:
+   1. Apply "gravity".
+   2. Apply viscosity impulses.
+   3. Advance to predicted position.
+   4. Update springs (Optional).
+   5. Apply spring displacements (Optional).
+   6. Apply Double Density Relaxation.
+   7. Resolve collisions.
+   8. Compute next velocity.
+
--- c/main.c
+++ c/main.c
@@ -1,16 +1,22 @@
-#include "SDL3/SDL_rect.h"
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include "SDL3/SDL_stdinc.h"
-#include "SDL3/SDL_log.h"
-#include "SDL3/SDL_hints.h"
-#include "SDL3/SDL_render.h"
+#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_hints.h>
+#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_timer.h>
-#include <string.h>
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_blendmode.h>
 
-#define PCOUNT 60
-#define GRAVITY 9.81
+#include <stdbool.h>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define TARGET_FPS 60
+#define MILLI_PER_FRAME ( 1000.0 / TARGET_FPS )
+
+#define PCOUNT 1000
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -23,14 +29,74 @@ typedef struct Particle_T {
 } Particle;
 
 Particle particles[PCOUNT];
-Uint64 neighbours[PCOUNT];
-Uint64 neighbour_count = 0;
-double interaction_radius = 40;
-Uint64 old_time = 0;
+Uint64 old_time;
+
+double interaction_radius;
+double bin_size;
+bool draw_grid;
+
+void initialize(void) {
+    SDL_Log("\x1b[2J"); // Clear screen.
+
+    for (int i = 0; i < PCOUNT; i++) {
+        particles[i].x = SDL_randf() * WINDOW_WIDTH;
+        particles[i].y = SDL_randf() * WINDOW_HEIGHT;
+        particles[i].vx = SDL_randf() * 2.0 - 1.0;
+        particles[i].vy = SDL_randf() * 2.0 - 1.0;
+    }
+
+    old_time = SDL_GetTicksNS();
+    interaction_radius = 20.0;
+    bin_size = (interaction_radius * 2.0) / 3.0;
+    draw_grid = true;
+}
+
+void draw(void) {
+    // Render the particles
+    SDL_SetRenderDrawColor(renderer, 96, 61, 31, 255);
+    SDL_RenderClear(renderer);
+    SDL_FRect rect;
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 16);
+    rect.y = 0;
+    rect.w = 2;
+    rect.h = WINDOW_HEIGHT;
+    for (int x = 0; x < WINDOW_WIDTH; x += bin_size) {
+        rect.x = x;
+        SDL_RenderFillRect(renderer, &rect);
+    }
+    rect.x = 0;
+    rect.w = WINDOW_WIDTH;
+    rect.h = 2;
+    for (int y = 0; y < WINDOW_HEIGHT; y += bin_size) {
+        rect.y = y;
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
+    SDL_SetRenderDrawColor(renderer, 55, 75, 178, 255);
+    rect.w = 5;
+    rect.h = 5;
+    for (int i = 0; i < PCOUNT; i++) {
+        rect.x = particles[i].x - (rect.w / 2.0);
+        rect.y = particles[i].y - (rect.h / 2.0);
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
+void iterate(double dt) {
+    for (int i = 0; i < PCOUNT; i++) {
+        particles[i].x += particles[i].vx * dt;
+        particles[i].y += particles[i].vy * dt;
+    }
+}
+
+
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    SDL_SetAppMetadata("Example HUMAN READABLE NAME", "1.0", "com.example.CATEGORY-NAME");
+    SDL_SetAppMetadata("CSCI40460U-FINAL", "1.0", "net.ontariotechu.jaime.barillas.CSCI4060U-FINAL");
     SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -38,21 +104,15 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/CATEGORY/NAME", 640, 480, 0, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("CSCI4060U - Final", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    SDL_SetRenderVSync(renderer, 1);
 
-    memset(particles, 0, sizeof(particles));
-    memset(neighbours, 0, sizeof(neighbours));
-    for (int i = 0; i < PCOUNT; i++) {
-        particles[i].x = SDL_randf() * 60;
-        particles[i].y = SDL_randf() * 40;
-        particles[i].vx = 0.0;
-        particles[i].vy = 0.0;
-    }
-    old_time = SDL_GetTicksNS();
+    SDL_SetRenderVSync(renderer, 1);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    initialize();
+
     return SDL_APP_CONTINUE;
 }
 
@@ -67,130 +127,18 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
     Uint64 new_time = SDL_GetTicksNS();
-    double dt = (float)(new_time - old_time) / 1000000000;
+    Uint64 dt = new_time - old_time;
     old_time = new_time;
+    SDL_Log("\x1b[H\x1b[0KFPS: %.2f", 1.0e9 / dt);
+    SDL_Log("\x1b[0KLast Frame Time: %.2fms", dt / 1.0e6);
 
-    for (int i = 0; i < PCOUNT; i++) {
-        // 1. Apply Gravity to Velocity
-        particles[i].vy += dt * GRAVITY;
+    iterate(dt / 1.0e9);
+    draw();
 
-        // 3. Advance to predicted Position
-        double oldx = particles[i].x;
-        double oldy = particles[i].y;
-        particles[i].x += dt * particles[i].vx;
-        particles[i].y += dt * particles[i].vy;
-
-        // 6. Double Density Relaxation
-        double density = 0;
-        double density_near = 0;
-        double pressure = 0;
-        double pressure_near = 0;
-
-        // Compute neighbours: O(n^2)
-        neighbour_count = 0;
-        for (int j = 0; j < PCOUNT; j++) {
-            if (j == i) continue;
-            double rx = particles[j].x - particles[i].x;
-            double ry = particles[j].y - particles[i].y;
-            double r = SDL_sqrt(rx*rx + ry*ry);
-
-            // Find insertion point.
-            for (Uint64 k = 0; k < PCOUNT; k++) {
-                double dx = particles[neighbours[k]].x - particles[i].x;
-                double dy = particles[neighbours[k]].y - particles[i].y;
-                double d = SDL_sqrt(dx*dx + dy*dy);
-                SDL_Log("i: %d, j: %d, r: %f, d: %f\n", i, j, r, d);
-
-                if (r < d) {
-                    for (Uint64 l = PCOUNT - 1; l > k; l--) {
-                        neighbours[l] = neighbours[l - 1];
-                    }
-                    neighbours[k] = j;
-                    neighbour_count += 1;
-                    break;
-                }
-            }
-        }
-
-        for (Uint64 j = 0; j < neighbour_count; j++) {
-            double rx = particles[neighbours[j]].x - particles[i].x;
-            double ry = particles[neighbours[j]].y - particles[i].y;
-            double r = SDL_sqrt(rx*rx + ry*ry);
-
-            double dterm = 1.0 - (r / interaction_radius);
-            density += dterm * dterm;
-            density_near += dterm * dterm * dterm;
-        }
-        double k = 0.5;
-        double k_near = 0.5;
-        double rest_density = 0.5;
-        pressure = k * (density - rest_density);
-        pressure_near = k_near * density_near;
-        double dx = 0;
-        double dy = 0;
-        SDL_Log("i: %d, Ns:%llu\n", i, neighbour_count);
-        for (Uint64 j = 0; j < neighbour_count; j++) {
-            double rx = particles[neighbours[j]].x - particles[i].x;
-            double ry = particles[neighbours[j]].y - particles[i].y;
-            double r = SDL_sqrt(rx*rx + ry*ry);
-            // SDL_Log("i: %d, j:%llu, rx: %f, ry: %f, r: %f\n", i, j, rx, ry, r);
-
-            if (r / interaction_radius < 1.0) {
-                rx /= r;
-                ry /= r;
-
-                double dterm = 1.0 - (r / interaction_radius);
-                double D_scale = (dt*dt) * ((pressure * dterm) + (pressure_near * dterm * dterm));
-                double Dx = D_scale * rx;
-                double Dy = D_scale * ry;
-
-                particles[neighbours[j]].x += Dx / 2;
-                particles[neighbours[j]].y += Dy / 2;
-                dx -= Dx / 2;
-                dy -= Dy / 2;
-            }
-        }
-        particles[i].x += dx;
-        particles[i].y += dy;
-        SDL_Log("dx: %f, dy: %f\n", dx, dy);
-
-        // 8. Compute next Velocity
-        particles[i].vx = (particles[i].x - oldx) / dt;
-        particles[i].vy = (particles[i].y - oldy) / dt;
-
-        // 9. Wrap arround window
-        if (particles[i].y > 480) {
-            particles[i].y = 0;
-        }
-    }
-
-    // 2. Apply Viscosity Impulses
-    // ...
-
-        // 4. Update Springs (Optional)
-        // ...
-
-    // 5. Apply Spring Displacements (Optional)
-    // ...
-
-    // 7. Resolve Collisions
-    // ...
-
-    // Render the particles
-    SDL_SetRenderDrawColorFloat(renderer, 0.8, 0.5, 0.6, 1.0);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColorFloat(renderer, 0.6, 0.5, 0.8, 1.0);
-    SDL_FRect rect;
-    rect.w = 5;
-    rect.h = 5;
-    for (int i = 0; i < PCOUNT; i++) {
-        rect.x = particles[i].x;
-        rect.y = particles[i].y;
-        SDL_RenderFillRect(renderer, &rect);
-    }
-
-    SDL_RenderPresent(renderer);
-    SDL_DelayNS(((1e9 * 16) / 1000) - (new_time - old_time));
+    // if ((dt / 1.0e6) < MILLI_PER_FRAME)
+    // {
+    //     SDL_DelayNS(MILLI_PER_FRAME - (dt / 1.0e6));
+    // }
     return SDL_APP_CONTINUE;
 }
 
