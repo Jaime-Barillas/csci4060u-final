3215245fed5044fc896d057d338e3024f14cc71d
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2025-02-16 02:30:34 -0500

    Sort into bins

--- c/main.c
+++ c/main.c
@@ -10,6 +10,8 @@
 #include <SDL3/SDL_blendmode.h>
 
 #include <stdbool.h>
+#include <stdint.h>
+#include <string.h> // memset().
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
@@ -29,7 +31,7 @@ typedef struct Particle_T {
 } Particle;
 
 Particle particles[PCOUNT];
-Uint64 old_time;
+uint64_t old_time;
 
 double interaction_radius;
 double bin_size;
@@ -47,7 +49,7 @@ void initialize(void) {
 
     old_time = SDL_GetTicksNS();
     interaction_radius = 20.0;
-    bin_size = (interaction_radius * 2.0) / 3.0;
+    bin_size = (interaction_radius * 2.0);
     draw_grid = true;
 }
 
@@ -85,7 +87,51 @@ void draw(void) {
     SDL_RenderPresent(renderer);
 }
 
+size_t bin_index(int32_t x, int32_t y, int32_t width, int32_t height) {
+    // FIXME: Handle negative x & y values, wrap around max index: (width * height).
+    int32_t b_size = (int32_t)bin_size;
+    return ((width * (y / b_size)) + (x / b_size)); // % (width * height);
+}
+
+void sort_into_bins(Particle ps[PCOUNT]) {
+    // Counting + Cycle sort. Sorts particles based on the grid cell (bin) they
+    // belong to using its index as their key.
+    int32_t grid_width = (int32_t)SDL_ceil(WINDOW_WIDTH / bin_size);
+    int32_t grid_height = (int32_t)SDL_ceil(WINDOW_HEIGHT / bin_size);
+    size_t num_cells = (grid_width * grid_height);
+    size_t count[num_cells];
+    memset(count, 0, num_cells * sizeof(size_t));
+
+    for (size_t i = 0; i < PCOUNT; i++) {
+        int32_t x = (int32_t)ps[i].x;
+        int32_t y = (int32_t)ps[i].y;
+        size_t key = bin_index(x, y, grid_width, grid_height);
+        count[key] += 1;
+    }
+
+    for (size_t i = 1; i < num_cells; i++) {
+        count[i] += count[i - 1];
+    }
+
+    // In-place cycle sort.
+    // See: https://stackoverflow.com/a/15719967
+    // See: https://stackoverflow.com/a/62598539
+    Particle temp;
+    for (size_t i = 0; i < PCOUNT; i++) {
+        int32_t x = (int32_t)ps[i].x;
+        int32_t y = (int32_t)ps[i].y;
+        size_t key = bin_index(x, y, grid_width, grid_height);
+        while (i < count[key] - 1) {
+            temp = ps[count[key] - 1];
+            ps[count[key] - 1] = ps[i];
+            ps[i] = temp;
+            count[key] -= 1;
+        }
+    }
+}
+
 void iterate(double dt) {
+    sort_into_bins(particles);
     for (int i = 0; i < PCOUNT; i++) {
         particles[i].x += particles[i].vx * dt;
         particles[i].y += particles[i].vy * dt;
@@ -126,8 +172,8 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    Uint64 new_time = SDL_GetTicksNS();
-    Uint64 dt = new_time - old_time;
+    uint64_t new_time = SDL_GetTicksNS();
+    uint64_t dt = new_time - old_time;
     old_time = new_time;
     SDL_Log("\x1b[H\x1b[0KFPS: %.2f", 1.0e9 / dt);
     SDL_Log("\x1b[0KLast Frame Time: %.2fms", dt / 1.0e6);
