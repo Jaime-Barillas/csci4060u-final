ed4c71e6e662b5cbca0547c3b015f5cf29011eee
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2025-06-08 16:48:10 -0400

    fix: Add sdl3 wrapper code

--- c/sdl3/lib.c
+++ c/sdl3/lib.c
@@ -1,1 +1,4 @@
-
+/* NOTE: Very hacky and potentially dangerous!
+ * Abuse Pony's return-type polymorphism to "cast" an SDL_Event to a specific
+ * event type. */
+void * PSDL_ConvertEvent(void * const event) { return event; }
