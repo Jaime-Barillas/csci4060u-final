b73f3e245ff3eb4211a6d94e3b220f416ab563ed
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2025-06-10 01:13:37 -0400

    chore: Remove useless qualifier

--- c/sdl3/lib.c
+++ c/sdl3/lib.c
@@ -1,4 +1,4 @@
 /* NOTE: Very hacky and potentially dangerous!
  * Abuse Pony's return-type polymorphism to "cast" an SDL_Event to a specific
  * event type. */
-void * PSDL_ConvertEvent(void * const event) { return event; }
+void * PSDL_ConvertEvent(void * event) { return event; }
